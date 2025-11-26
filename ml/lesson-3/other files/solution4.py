6a44bcb5616b1369276defc698eef1cf276625aa
Hikmatyor Azatov <azatovhikmatyor@gmail.com>
2025-11-26 12:33:45 +0500

    initial

---
+++ ml/lesson-3/other files/solution4.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+
+df = pd.read_csv("data/housing.csv")
+print(df.head())
+print(df.info())
+print(df.describe())
